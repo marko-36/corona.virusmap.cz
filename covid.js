b5babb249f54a21e387741c15c6e65290b31463a
marko-36 <martin.korvas@gmail.com>
2020-03-17 11:34:59 +0100

    Handle unmatched countries

--- covid.js
+++ covid.js
@@ -267,15 +267,19 @@ async function covid() {
     $(dataSelectors.rows, freshDataHTML).each(function () {
         currentRow = $(this);
         country = localNames[$(dataSelectors.countryName, currentRow).text().trim()]
-        data[country].data[now] = {}
-        freshData = data[country].data[now]
-        freshData.when = new Date().getTime()
-        freshData.cases = $(dataSelectors.cases, currentRow).text().trim()
-        freshData.deceased = $(dataSelectors.deceased, currentRow).text().trim()
-        freshData.recovered = $(dataSelectors.recovered, currentRow).text().trim()
-        if (freshData.cases == "") {freshData.cases = 0}
-        if (freshData.deceased == "") {freshData.deceased = 0}
-        if (freshData.recovered == "") {freshData.recovered = 0}
+        if (country == undefined) {
+            //log the error
+        } else {
+            data[country].data[now] = {}
+            freshData = data[country].data[now]
+            freshData.when = new Date().getTime()
+            freshData.cases = $(dataSelectors.cases, currentRow).text().trim()
+            freshData.deceased = $(dataSelectors.deceased, currentRow).text().trim()
+            freshData.recovered = $(dataSelectors.recovered, currentRow).text().trim()
+            if (freshData.cases == "") {freshData.cases = 0}
+            if (freshData.deceased == "") {freshData.deceased = 0}
+            if (freshData.recovered == "") {freshData.recovered = 0}
+        }
     })
     fs.writeFile('_data.json', JSON.stringify(data), function () {})
 }
