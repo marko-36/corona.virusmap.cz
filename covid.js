70e905ed157686faaacdc549296ca9708a41af5f
marko-36 <martin.korvas@gmail.com>
2020-03-18 11:33:32 +0100

    recountData() for front-end done. Types in _data.json fixed. Local source used.

--- covid.js
+++ covid.js
@@ -25,12 +25,33 @@ async function getLiveURL(url){
   html = await page.content();
   browser.close();
   return html
-};     
+};
+
+function recountData(){  // move to front-end
+  const data = JSON.parse(fs.readFileSync('_data.json', 'utf8'));
+  for (let country of Object.keys(data)){
+    for (let dataChunk of Object.keys(data[country].data)){
+      dataPoint = data[country].data[dataChunk]
+      dataPoint.p_cases = dataPoint.cases / data[country].pop
+      dataPoint.p_dec = dataPoint.deceased / data[country].pop
+      dataPoint.p_rec = dataPoint.recovered / data[country].pop
+      dataPoint.a_cases = dataPoint.cases / data[country].area
+      dataPoint.a_dec = dataPoint.deceased / data[country].area
+      dataPoint.a_rec = dataPoint.recovered / data[country].area
+    }
+  }
+  fs.writeFile('_data.json', JSON.stringify(data), function(){console.log('recountData done!')})     
+}
+
+function data2graph(data){  // regroups data for use in graphs; move to front-end
+
+}
 
 async function covid(logFn,deleteWhen) {
   const now = new Date()
   const data = JSON.parse(fs.readFileSync('_data.json', 'utf8'));
-  const freshDataHTML = await getLiveURL(privateDataSources.src01.url);  
+  //const freshDataHTML = await getLiveURL(privateDataSources.src01.url);  
+  const freshDataHTML = fs.readFileSync('_src.json', 'utf8')   
   const dataSelectors = privateDataSources.src01.selectors;    
 
   if (deleteWhen){
@@ -46,17 +67,19 @@ async function covid(logFn,deleteWhen) {
       } else {
         data[country].data[now] = {}      
         freshData = data[country].data[now]
-        freshData.when = new Date().getTime()
-        freshData.cases = $(dataSelectors.cases, currentRow).text().trim()
-        freshData.deceased = $(dataSelectors.deceased, currentRow).text().trim()
-        freshData.recovered =  $(dataSelectors.recovered, currentRow).text().trim()
-        if (freshData.cases == "") {freshData.cases = 0}
-        if (freshData.deceased == "") {freshData.deceased = 0}
-        if (freshData.recovered == "") {freshData.recovered = 0}
+        freshData.when = now.getTime()
+        freshData.cases = parseInt($(dataSelectors.cases, currentRow).text().trim().replace(",", ""))
+        freshData.deceased = parseInt($(dataSelectors.deceased, currentRow).text().trim().replace(",", ""))
+        freshData.recovered =  parseInt($(dataSelectors.recovered, currentRow).text().trim().replace(",", ""))
+        if (isNaN(freshData.cases)) {freshData.cases = 0}
+        if (isNaN(freshData.deceased)) {freshData.deceased = 0}
+        if (isNaN(freshData.recovered)) {freshData.recovered = 0}
       }
     })
     logFn('EOF','covid.log')
   }
   fs.writeFile('_data.json', JSON.stringify(data), function(){console.log('done!')})       
 }
-covid(log)
+
+//covid(log)
+recountData()
